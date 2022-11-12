53e6ebc5d0cf7d1b0f7b23a7eb2ad268ff272fb0
alexander1fein <feinberg.alexander@gmail.com>
2022-11-11 23:46:39 -0500

    POST stories

--- app/api/story_routes.py
+++ app/api/story_routes.py
@@ -1,11 +1,23 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from ..models.story import Story
 from ..errors import NotFoundError
 from ..forms.story_form import StoryForm
 from ..models import db
+from datetime import datetime
 
 story_routes = Blueprint('stories', __name__)
+
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field}:{error}')
+    return errorMessages
 
 
 @story_routes.route('/')
@@ -22,6 +34,12 @@ def all_stories():
 @login_required
 def create_story():
     form = StoryForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        print("FORM DATA ", form.data)
-        new_story = Story(user=current_user, **form.data)
+        new_story = Story(user=current_user,
+                          title=form.data['title'],
+                          image=form.data['image'],
+                          content=form.data['content'],
+                          created_at=datetime.now())
+        return jsonify(new_story.to_dict())
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 401
