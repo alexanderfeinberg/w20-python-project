e19db807b77fe9f977c7cc4fba7e7b31a76267f0
alexander1fein <feinberg.alexander@gmail.com>
2022-11-12 11:01:11 -0500

    Follower count on user model

--- app/models/user.py
+++ app/models/user.py
@@ -56,5 +56,6 @@ class User(db.Model, UserMixin):
             'firstName': self.first_name,
             'lastName': self.last_name,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'followerCount': len(follows)
         }
