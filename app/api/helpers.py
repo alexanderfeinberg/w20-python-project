bdd56208d7d7635bfa2e2cdee84d5af9456c1f65
alexander1fein <feinberg.alexander@gmail.com>
2022-11-12 00:52:26 -0500

    User model helper

--- app/api/helpers.py
+++ app/api/helpers.py
@@ -4,3 +4,7 @@ from ..errors import ForbiddenError
 def child_belongs_to_parent(parent_model, child_model, child_foreign_col: str):
     if parent_model.id != getattr(child_model, child_foreign_col):
         raise ForbiddenError("User does not have access.")
+
+
+def get_user_model(current_user, user_model):
+    return user_model.query.get(current_user.id)
