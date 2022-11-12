51fee18d02c7ea37b5734741402b41c577bbcdf3
alexander1fein <feinberg.alexander@gmail.com>
2022-11-12 17:14:12 -0500

    Edit comment

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, request, jsonify
-from flask_login import login_required
-from app.models import Comment, db
+from flask_login import login_required, current_user
+from app.models import Comment, db, User
 from app.forms.comment_form import CommentForm
 from datetime import datetime
 from app.errors import NotFoundError
+from .helpers import child_belongs_to_parent, get_user_model
+
 comment_routes = Blueprint('comments', __name__)
 
-# Edit a Comment 
+# Edit a Comment
+
+
 @comment_routes.route('/<int:comment_id>', methods=["PUT"])
 @login_required
 def edit_comment(comment_id):
@@ -15,12 +19,13 @@ def edit_comment(comment_id):
     if form.validate_on_submit():
         data = form.data
         edited_comment = Comment.query.get(comment_id)
+        child_belongs_to_parent(get_user_model(current_user, User), edited_comment, 'user_id')
     if edited_comment:
         edited_comment.content = data['content']
         edited_comment.created_at = datetime.now()
         db.session.commit()
         return jsonify(edited_comment.to_dict())
-    else: 
+    else:
         return NotFoundError("Comment can't be found")
 
 
@@ -29,10 +34,9 @@ def edit_comment(comment_id):
 @login_required
 def delete_comment(comment_id):
     deleted_comment = Comment.query.get(comment_id)
-    if delete_comment: 
+    if delete_comment:
         db.session.delete(deleted_comment)
         db.session.commit()
         return {"message": "Comment successfully deleted."}
-    else: 
+    else:
         return NotFoundError("Comment couldn't be found")
-
