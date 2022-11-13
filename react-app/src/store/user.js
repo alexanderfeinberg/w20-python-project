c55e7302a01c650812ab17995745309df8229080
alexander1fein <feinberg.alexander@gmail.com>
2022-11-13 11:27:23 -0500

    Actions

--- react-app/src/store/user.js
+++ react-app/src/store/user.js
@@ -1,15 +1,19 @@
+import { csrfFetch } from "./csrf";
 
-
-
+const LOAD_USER = "/users/LOAD_USER";
+const LOAD_CURRENT_USER = "/users/LOAD_CURRENT_USER";
+const LOAD_USER_LIST = "/users/LOAD_USER_LIST";
+const FOLLOW_USER = "/users/FOLLOW_USER";
+const UNFOLLOW_USER = "/users/UNFOLLOW_USER";
 
 initialState = {
-    singleUser: {},
-    userList: {}
-}
+  singleUser: {},
+  userList: {},
+};
 
 export const userReducer = (state = initialState, action) => {
-    switch (action.type) {
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
