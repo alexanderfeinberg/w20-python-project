979ceeb953fc814b0368de4aff1f7be89b695fc7
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-11-14 14:23:22 -0800

    added css

--- react-app/src/components/GetAllStories/GetAllStories.js
+++ react-app/src/components/GetAllStories/GetAllStories.js
@@ -1,16 +1,26 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory, useParams } from "react-router-dom";
 import { getAllStories } from '../../store/story';
+import { getUser, getUserList } from "../../store/user";
 import './GetAllStories.css';
 
 const GetAllStories = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
+  const { userId }  = useParams();
+
   const stories = useSelector(state => state.story.allStories);
   const storiesArr = Object.values(stories);
 
+  const user = useSelector(state => state.user.singleUser)
+
   useEffect(() => {
     dispatch(getAllStories())
+  }, [dispatch]);
+ 
+  useEffect(() => {
+    dispatch(getUser())
   }, [dispatch]);
 
    if (Object.keys(storiesArr).length === 0) {
@@ -23,25 +33,27 @@ const GetAllStories = () => {
         {storiesArr && storiesArr.map((story) => {
           return (
             <div className="stories-container2">
-              <div className="stories-author-info">{story.author.profile_picture} {story.author.firstName} {story.author.lastName} · {story.createdAt.slice(5, 11)}</div>
-              <div className="stories-wrapper">
-                <NavLink key={story.id} to={`/stories/${story.id}`}>
+                <div className="stories-author-info">{story.author.profile_picture} {story.author.firstName} {story.author.lastName} · {story.createdAt.slice(5, 11)}
+                  {/* <NavLink key={user.id} to={`/users/${user.id}`}></NavLink> */}
+                  {/* <NavLink onClick={() => history.push(`/users/${user.id}`)}></NavLink> */}
+                </div>
+                <div className="stories-info">
+                  <NavLink key={story.id} to={`/stories/${story.id}`}>
 
-                <div className="stories-container-left">
-                    <div className="stories_title">{story.title}</div>
-                    <div className="stories-content">{story.content}</div>
+                  <div className="stories-container-left">
+                      <div className="stories_title">{story.title}</div>
+                      <div className="stories-content">{story.content}</div>
+                  </div>
+                    
+                  <div className="stories-container-right">
+                    <img 
+                        className="stories-image"
+                        src={"https://cdn.pixabay.com/photo/2022/11/01/05/18/coffee-7561288_1280.jpg"}
+                        alt=""
+                    />
+                  </div>
+                  </NavLink>
                 </div>
-                  
-                <div className="stories-container-right">
-                  <h1>Image Section</h1>
-                  {/* <img 
-                      className="stories-image"
-                      src={"https://cdn.pixabay.com/photo/2022/11/01/05/18/coffee-7561288_1280.jpg"}
-                      alt=""
-                  /> */}
-                </div>
-                </NavLink>
-              </div>
           </div>
           )
         })}
--- react-app/src/components/GetOneStory/GetOneStory.js
+++ react-app/src/components/GetOneStory/GetOneStory.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, NavLink } from "react-router-dom";
 import { getSingleStory } from '../../store/story';
+import commentIcon from "../../assets/comment-icon.png";
+import likeIcon from "../../assets/like-icon.jpeg";
 import './GetOneStory.css';
 
 const GetOneStory = () => {
@@ -26,16 +28,24 @@ const GetOneStory = () => {
     <>
     <div className="containers">
       <div className="container-2">
-        <div className="story-author">{story.author.firstName} {story.author.lastName}</div>
-        <div className="story_title">{story.title}</div>
+        <div className="story-info">
+        <div className="story-header">
+          <div className="story-author-info">{story.author.firstName} {story.author.lastName}</div>
+          <div className="story-author-info2"> {story.createdAt.slice(5, 11)}</div>
+        </div>
+        <div className="story-title">{story.title}</div>
         <img 
             className="story-image"
             src={"https://cdn.pixabay.com/photo/2022/11/01/05/18/coffee-7561288_1280.jpg"}
             alt=""
         />        
         <div className="story-content">{story.content}</div>
-        <div className="story-likes">{story.likeCount}</div>
-        <div className="story-comments">{story.commentCount}</div>
+        <div className="story-likes-comment">
+          <img className="like-icon" src={likeIcon} alt="Like Icon"/> 
+            <span className="story-likes">{story.likeCount}</span>
+          <img className="comment-icon" src={commentIcon} alt="Comment Icon" /> {story.commentCount}      
+        </div>
+        </div>
       </div>
       <div className="container-3">3rd Section</div>
     </div>
