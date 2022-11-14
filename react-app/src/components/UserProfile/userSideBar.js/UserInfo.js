6692dec87ed73e341b6c4be010de4e3797ccacbd
alexander1fein <feinberg.alexander@gmail.com>
2022-11-13 23:55:18 -0500

    Added followings to user store

--- react-app/src/components/UserProfile/userSideBar.js/UserInfo.js
+++ react-app/src/components/UserProfile/userSideBar.js/UserInfo.js
@@ -1,4 +1,15 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { loadFollowings } from "../../../store/user";
+
 const UserInfo = (user) => {
+  const followings = useSelector((state) => state.user.userList);
+  const dispatch = useDispatch();
+  const [isLoaded, setisLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(loadFollowings(user.id).then(() => setisLoaded(true)));
+  }, [user.id]);
   return (
     <div>
       <div>
