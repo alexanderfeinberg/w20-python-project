355af10ea3e76a124b6aef9cd8cca64f1aa66ca7
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-11-13 17:53:47 -0800

    added users stories components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,31 +3,25 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import NavBar from './components/NavBar';
 import { authenticate } from './store/session';
-import GetAllStories from './components/GetAllStories/GetAllStories';
-import GetOneStory from './components/GetOneStory/GetOneStory';
-import GetUsersStories from './components/GetUsersStories/GetUsersStories';
+import './App.css'
+
+import Home from "./components/home"
 
 function App() {
   const dispatch = useDispatch();
+  const user = useSelector(state => state.session.user)
   useEffect(() => {
     dispatch(authenticate())
   }, [dispatch])
   return (
-    <BrowserRouter>
-      <NavBar />
-      <h1>My Home Page</h1>
-    <Switch>
-      <Route path='/stories/:storyId'>
-        <GetOneStory />
-      </Route>
-      <Route path='/users/:userId/stories'>
-        <GetUsersStories />
-      </Route>
-      <Route path='/'>
-        <GetAllStories />
-      </Route>
-    </Switch>
-    </BrowserRouter>
+    <div id={user ? "app-container" : "app-container-logged-out"}>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
