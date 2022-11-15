e506a0c06c147b111a8442d6e27108c94a14c8ee
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-11-15 12:10:11 -0800

    added edit/delete story

--- react-app/src/components/GetAllStories/GetAllStories.js
+++ react-app/src/components/GetAllStories/GetAllStories.js
@@ -1,24 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useHistory, useParams } from "react-router-dom";
-import { getAllStories } from '../../store/story';
-import { getUser, getUserList } from "../../store/user";
+import { deleteStory, getSingleStory } from "../../store/story";
+import { getUser } from "../../store/user";
 import './GetAllStories.css';
 
 const GetAllStories = ({stories}) => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const { storyId } = useParams();
 
+  const user = useSelector((state) => state.session.user);
   const storiesArr = Object.values(stories);
 
+  const [showMenu, setShowMenu] = useState(false);
 
   useEffect(() => {
     dispatch(getUser())
   }, [dispatch]);
 
-   if (Object.keys(storiesArr).length === 0) {
+    const openMenu = () => {
+      if (showMenu) return;
+      setShowMenu(true);
+    };
+    
+    useEffect(() => {
+      if (!showMenu) return;
+  
+      const closeMenu = () => {
+        setShowMenu(false);
+      };
+  
+      document.addEventListener('click', closeMenu);
+    
+      return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+  
+    
+  if (Object.keys(storiesArr).length === 0) {
     return null;
   }
+
+  const deleteHandler = (storyId) => {
+    dispatch(deleteStory(storyId));
+    history.push("/");
+  };
 
   return (
     storiesArr && (
@@ -27,14 +53,24 @@ const GetAllStories = ({stories}) => {
           return (
             <div className="stories-container2">
                 <div className="stories-info">
-                  <div className="stories-author-info">{story.author.profile_picture} {story.author.firstName} {story.author.lastName} · {story.createdAt.slice(5, 11)}
-                    {/* <NavLink key={user.id} to={`/users/${user.id}`}></NavLink> */}
-                    {/* <NavLink onClick={() => history.push(`/users/${user.id}`)}></NavLink> */}
+                  <div className="stories-author-info" onClick={() => history.push(`/users/${user.id}`)}>{story.author.profile_picture} {story.author.firstName} {story.author.lastName} · {story.createdAt.slice(5, 11)}
+                    <NavLink key={user.id} to={`/users/${user.id}`}></NavLink>
                   </div>
                   <NavLink key={story.id} to={`/stories/${story.id}`}>
                       <div className="stories-title">{story.title}</div>
                     <div className="stories-content">{story.content}</div>
                   </NavLink>
+                  {user && showMenu &&  <button type='button' className="stories-options-dropdown">
+                  <i className="fa-solid fa-ellipsis"></i>
+                    {user && <button className="edit-story-button"
+                      onClick={() => history.push(`/story/${story.id}/edits`)}>
+                      Edit story
+                    </button>}
+                    {user && <button className="delete-story-button"
+                      onClick={() => deleteHandler(story.id)}>
+                      Delete story
+                    </button>}
+                  </button>}
                 </div>  
 
                   <div className="stories-image-container">
@@ -44,7 +80,6 @@ const GetAllStories = ({stories}) => {
                         alt=""
                     />
                   </div>
-                  <i className="fa-solid fa-ellipsis"></i>
                 </div>
           )
         })}
