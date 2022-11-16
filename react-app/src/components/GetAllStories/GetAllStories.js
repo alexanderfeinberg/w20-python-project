a7f0ed4b24af40118db92cf0807727cfc6966ffd
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-11-16 10:34:42 -0800

    css added to all stories

--- react-app/src/components/GetAllStories/GetAllStories.js
+++ react-app/src/components/GetAllStories/GetAllStories.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink, useHistory, useParams } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 import { deleteStory } from "../../store/story";
 import { getUser } from "../../store/user";
 import profileIcon from "../../assets/profile-icon.jpeg";
@@ -9,14 +9,9 @@ import './GetAllStories.css';
 const GetAllStories = ({stories}) => {
   const dispatch = useDispatch();
   const history = useHistory();
-  const { userId } = useParams();
 
   const user = useSelector((state) => state.session.user);
   const storiesArr = Object.values(stories);
-
-  useEffect(() => {
-    dispatch(getUser(userId))
-  }, [dispatch, userId]);
 
   if (Object.keys(storiesArr).length === 0) {
     return null;
