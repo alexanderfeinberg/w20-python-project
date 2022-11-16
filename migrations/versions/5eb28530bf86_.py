f22817d0cae57170f52b2260a5395eef0595f78f
alexander1fein <feinberg.alexander@gmail.com>
2022-11-16 00:22:06 -0500

    Fixing follower logged out bug

--- migrations/versions/5eb28530bf86_.py
+++ migrations/versions/5eb28530bf86_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 69a5d1566419
+Revision ID: 5eb28530bf86
 Revises: 
-Create Date: 2022-11-15 23:58:22.221932
+Create Date: 2022-11-16 00:04:56.412265
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '69a5d1566419'
+revision = '5eb28530bf86'
 down_revision = None
 branch_labels = None
 depends_on = None
